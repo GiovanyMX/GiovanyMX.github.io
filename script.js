5d4d49c086038c6688c129002d6c8e403db50704
GiovanyMX <86213786+GiovanyMX@users.noreply.github.com>
2021-06-29 00:40:21 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuButton = document.getElementById("menuButt");
-const menuSubMenu = document.getElementById("menuSubMenu");
-
-menuButton.addEventListener("click", function(){
-    menuSubMenu.classList.toggle("show");
-})
